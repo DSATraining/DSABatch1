77460b520c27e694a20682f1a9cbc9a1ddb44689
DSATraining <dsatraining50@gmail.com>
2025-03-21 19:40:36 +0530

    Update Solutions.java

--- Backtracking/Solutions.java
+++ Backtracking/Solutions.java
@@ -53,3 +53,54 @@ class Solution {
         return true;
     }
 }
+
+
+
+
+//Code for rat in a maze
+class Solution {
+    // Function to find all possible paths
+    public ArrayList<String> findPath(ArrayList<ArrayList<Integer>> mat) {
+        // code here
+        int[][] mat2 = new int[mat.size()][mat.size()];
+        int m = mat.size();
+        for(int i = 0;i<m;i++){
+            for(int j = 0;j<m;j++){
+                mat2[i][j] = mat.get(i).get(j);
+            }
+        }
+        boolean[][] visited = new boolean[m][m];
+        ArrayList<String> ans = new ArrayList<>();
+        String tempAns = "";
+        int row = 0;
+        int col = 0;
+        backtrack(mat2,ans,tempAns,visited,row,col);
+        return ans;
+    }
+    public void backtrack(int[][] mat,ArrayList<String> ans, String p, boolean[][] visited, int r, int c){
+        if(r == mat.length-1 && c == mat.length - 1){
+            ans.add(p);
+            return;
+        }
+        //base cases
+        if(mat[r][c] == 0 || visited[r][c] == true){
+            return;
+        }
+        visited[r][c] = true;
+        //lets go down of the maze 1st
+        if(r < mat.length - 1)
+            backtrack(mat,ans,p + "D",visited,r + 1 ,c); //backtrack(mat, ans, p+"d",visited,0+1,0)
+        //lets go to the left of the maze after that
+        if(c > 0)
+            backtrack(mat, ans, p + "L", visited, r, c - 1);
+            
+        //next path is going right
+        if(c < mat.length - 1)
+            backtrack(mat, ans, p + "R", visited,r, c+1);
+        //only remaining path is up
+        if(r > 0)
+            backtrack(mat, ans, p + "U", visited,r-1,c );
+        //main backtraking code
+        visited[r][c] = false;
+    }
+}
